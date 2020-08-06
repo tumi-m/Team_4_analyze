364382cd169db21b861e09ccc5cf4601bb105a6c
tumi-m <tumeloxmalebo@gmail.com>
2020-08-06 08:41:55 +0200

    Hi team, uploaded function 5

--- Analyze sprint/script.py
+++ Analyze sprint/script.py
@@ -108,7 +108,10 @@ def extract_municipality_hashtags(df):
 ### START FUNCTION
 def number_of_tweets_per_day(df):
     # your code here
-    return
+"""Function which list number of tweets per day by converting pandas dataframe into a new dataframe specified by yyyy-mm-dd"""
+  df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')
+  return df.groupby('Date').count()
+
 
 ### END FUNCTION
 
